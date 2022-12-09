3466b932799e27bd04dbe2ce256589437cfe9404
Glenn <a810.glenn@gmail.com>
2022-12-09 16:40:27 +0900

    dynamic inline styles

--- src/components/CourseGoals/CourseGoalList/CourseGoalList.js
+++ src/components/CourseGoals/CourseGoalList/CourseGoalList.js
@@ -1,12 +1,10 @@
-import React from 'react';
+import CourseGoalItem from '../CourseGoalItem/CourseGoalItem'
+import './CourseGoalList.css'
 
-import CourseGoalItem from '../CourseGoalItem/CourseGoalItem';
-import './CourseGoalList.css';
-
-const CourseGoalList = props => {
+const CourseGoalList = (props) => {
   return (
     <ul className="goal-list">
-      {props.items.map(goal => (
+      {props.items.map((goal) => (
         <CourseGoalItem
           key={goal.id}
           id={goal.id}
@@ -16,7 +14,7 @@ const CourseGoalList = props => {
         </CourseGoalItem>
       ))}
     </ul>
-  );
-};
+  )
+}
 
-export default CourseGoalList;
+export default CourseGoalList
